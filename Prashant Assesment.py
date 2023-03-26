fcf975a31ff961d7b10cc25ab931a3f2ff1b62fa
Harakraj1995 <128920677+Harakraj1995@users.noreply.github.com>
2023-03-26 14:18:57 +0530

    Add files via upload
    Uploaded the file to public repository

---
+++ Prashant Assesment.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+# - In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# # Objectives
+# - * Some of the popular languages that Data Scientists
+# - * Some of the commonly used libraries used by Data Scientists include
+# - * Some of the commonly usedData Science Tools
+# - * evaluating arithmetic expressions in Python
+# 
+# 
+# - Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 1. Javascript
+# 1. R Language
+# 1. SQL
+# 
+
+# - Some of the commonly used libraries used by Data Scientists include:
+# 1. Scitic-Learn
+# 1. TensorFlow
+# 1. PyTorch
+
+# - Some of the commonly usedData Science Tools
+
+# | Data Science Tools |
+# ----
+# | Jupyter Notebook |
+# | GitHub |
+# | Rstudio |
+
+#  ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# - This a simple arithmetic expression to mutiply then add integers
+
+# # To multiply two numbers
+
+# In[4]:
+
+
+X =int(input("enter the value of X"))
+Y =int(input("enter the value of Y"))
+Z =int(input("enter the value of Z"))
+V = (X*Y)+Z
+print("Additon and multiplication is " , V)
+
+
+# - This will convert 200 minutes to hours by diving by 60 
+
+# In[7]:
+
+
+X =int(input("enter the value of X"))
+Y =int("60")
+Z = X / 60
+print("Conversion of minutes into hour is " , Z)
+
+
+# ## Authur
+# - Prashant R. Harak
+
+# In[ ]:
+
+
+
+
